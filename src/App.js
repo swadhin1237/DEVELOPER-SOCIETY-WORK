06da8eedc59af7d360037517eb348638e5fb7304
SWADHIN SATYAPRAKASH MAJHI <majhiswadhin64@gmail.com>
2022-11-04 19:54:19 +0530

    all complete

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header';
+import Centre from './Component/Center_portion'
+import Footer from './Component/Footer'
 
 function App() {
   return (
       <>
-
-
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/" >Welcome To KGP</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
-
-   
-      </ul>
-    </div>
-  </div>
-</nav>
+       <Header></Header>
+        <Centre></Centre> 
+        <Footer></Footer>
       </>
   );
 }
---
+++ src/Component/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+export default function footer() {
+    return (
+      <>
+      <footer className='footer p-10 bg-gray-700 text primary-content footer-center'>
+      <div>
+     
+   
+      </div>
+
+      </footer>
+      </>
+    )
+  }
+  
